ca4202c841d0b6b3ccf2aaed3e2f10936c30c887
desktop <kajal.giran1993@gmail.com>
2017-04-09 11:48:07 +0530

    jhjgh***********************8

--- app/src/main/java/com/trymash/Activity/HelpActivity.java
+++ app/src/main/java/com/trymash/Activity/HelpActivity.java
@@ -13,7 +13,7 @@ public class HelpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help);////
+        setContentView(R.layout.activity_help);////////*******************************
     }
 
     @Override
